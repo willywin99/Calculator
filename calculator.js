fcfe428e118752b71ca26583721da448a1673ff9
willywin99 <willywinsianturi@gmail.com>
2022-10-19 15:49:48 +0700

    feat: index.html Responding to Requests with HTML Files

--- calculator.js
+++ calculator.js
@@ -5,9 +5,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, function () {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 3000.');
 });
